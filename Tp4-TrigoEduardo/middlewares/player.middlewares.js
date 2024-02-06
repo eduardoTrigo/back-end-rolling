37417de618aef69eeb7382a4cf45d4d8cac4d4d4
Eduardo Trigo <eduatrigo@gmail.com>
2024-02-06 12:57:12 -0300

    ..

---
+++ Tp4-TrigoEduardo/middlewares/player.middlewares.js
@@ -0,0 +1,28 @@
+const { requestValidation } = require("./commonMiddleware")
+const { body, query } = require('express-validator')
+
+const validationMongoId = [
+    query('playerId').optional().isMongoId().withMessage('deberia ser un id formato mongo'),
+    requestValidation,
+]
+
+const validationPlayerData = [
+    body('nombre').notEmpty().withMessage('el nombre es requerido'),
+    body('nombre').isString().withMessage('el nombre debe ser alfabetico'),
+    body('valor').optional().isNumeric().withMessage('el valor debe ser numerico'),
+    body('estrellas').optional().isNumeric().withMessage('el valor debe ser numerico'),
+    requestValidation,
+]
+
+const validatePatchPlayer = [
+    body('playerId').optional().isString().withMessage('el nombre debe ser alfabetico'),
+    body('valor').optional().isNumeric().withMessage('el valor debe ser numerico'),
+    body('estrellas').optional().isNumeric().withMessage('el valor debe ser numerico'),
+    requestValidation,
+]
+
+module.exports = {
+    validationPlayerData,
+    validationMongoId,
+    validatePatchPlayer
+}
